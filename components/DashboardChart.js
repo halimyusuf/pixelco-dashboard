c622b592fc4b5e7124a214c19b4db1e3e8a393d4
halimyusuf <haleemyoosuph@gmail.com>
2022-03-29 13:57:42 +0100

    chore: changed grid color

--- components/DashboardChart.js
+++ components/DashboardChart.js
@@ -22,38 +22,52 @@ ChartJS.register(
   Legend
 );
 
+function generateRandom() {
+  const min = 0;
+  const max = 3;
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
 export const options = {
-  borderColor: function (context, options) {
-    const color = options.color; // resolve the value of another scriptable option: 'red', 'blue' or 'green'
-    return Chart.helpers.color(color).lighten(0.8);
-  },
   responsive: true,
   plugins: {
-    legend: {
-      position: "top",
-    },
-    title: {
-      display: true,
-      text: "Chart.js Line Chart",
+    // legend: {
+    //   position: "top",
+    // },
+    legend: false,
+  },
+  scales: {
+    // yAxes: {
+    //   grid: {
+    //     borderColor: "#eeeeee",
+    //     color: "#eeeeee",
+    //     tickColor: "#eeeeee",
+    //   },
+    // },
+    xAxes: {
+      grid: {
+        // borderColor: "#eeeeee",
+        color: "#eeeeee",
+      },
     },
   },
 };
 
-const labels = ["January", "February", "March", "April", "May", "June", "July"];
+const labels = ["Jan", "Feb", "Mar", "Apr", "May", "June", "July"];
 
 export const data = {
   labels,
-  color: "#eeeeee",
+  borderColor: "#ffffff",
   datasets: [
     {
-      label: "Dataset 1",
-      data: labels.map((a) => a.length),
+      // label: "Dataset 1",
+      data: labels.map((a) => a.length + generateRandom()),
       borderColor: "#DEA34B",
       backgroundColor: "rgba(255, 99, 132, 0.5)",
     },
     {
-      label: "Dataset 2",
-      data: labels.map((a) => a.length + 3),
+      // label: "Dataset 2",
+      data: labels.map((a) => a.length + generateRandom()),
       borderColor: "#AE2EEA",
       backgroundColor: "rgba(53, 162, 235, 0.5)",
     },
