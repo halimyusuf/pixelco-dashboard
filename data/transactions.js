b31ea922f4343449b9c38fbc102206dc89cc35fc
halimyusuf <haleemyoosuph@gmail.com>
2022-03-31 21:16:26 +0100

    chore: transactions data

---
+++ data/transactions.js
@@ -0,0 +1,5 @@
+export const transactions = [
+  { text: "School Expenses", amount: 257.09, deposit: false },
+  { text: "Transportation", amount: 278.92, deposit: false },
+  { text: "Apartment", amount: 893.62, deposit: true },
+];
