862ac8d7f0d9654c64834495252b9291d11f4383
halimyusuf <haleemyoosuph@gmail.com>
2022-03-31 16:31:25 +0100

    chore: income data

---
+++ data/incomeData.js
@@ -0,0 +1,16 @@
+export const incomeData = [
+  { month: "Jan", amount: 21000 },
+  { month: "Feb", amount: 13000 },
+  { month: "Mar", amount: 18000 },
+  { month: "Apr", amount: 22000 },
+  { month: "May", amount: 24000 },
+  { month: "June", amount: 13000 },
+  { month: "July", amount: 17000 },
+  { month: "Aug", amount: 25000 },
+  { month: "Sep", amount: 13000 },
+  { month: "Oct", amount: 27000 },
+  { month: "Nov", amount: 19000 },
+  { month: "Dec", amount: 16000 },
+];
+
+export const incomePrices = [0, 10000, 20000, 30000];
